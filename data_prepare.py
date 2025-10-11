21d440179851c2826fe601f80a9a15e1ba3803d3
i3boyacc <lini3acc@outlook.com>
2025-10-12 00:30:50 +0800

    Add files via upload

---
+++ data_prepare.py
@@ -0,0 +1,96 @@
+import numpy as np
+import pandas as pd
+import networkx as nx
+import os
+from utils import *
+import shutil
+
+for cancer in cancers:
+    ppi = pd.read_table('./datasets/ppi_network.txt',header=None)
+    
+    data = list()
+    data.append(pd.read_csv('./datasets/' + cancer + '/' + cancer + '_mut.csv',header=0,index_col=0))
+    data.append(pd.read_csv('./datasets/' + cancer + '/' + cancer + '_methy.csv',header=0,index_col=0))
+    data.append(pd.read_csv('./datasets/' + cancer + '/' + cancer + '_expr.csv',header=0,index_col=0))
+    
+    data[0] = data[0][data[0].mean(axis=1)>=mut_thres[cancer]]
+
+    data[1] = data[1][data[1].index.isin(data[0].index)]
+    data[2] = data[2][data[2].index.isin(data[1].index)]
+    ppi = ppi[ppi[0].isin(data[2].index)&ppi[1].isin(data[2].index)]
+    
+    G = nx.Graph()
+    G.add_edges_from(ppi.values)
+    
+    ppi_genes = sorted(nx.connected_components(G), key=len, reverse=True)[0]
+    ppi_genes = np.array(list(ppi_genes))
+    ppi_genes = ppi_genes.astype(str)
+    
+    data[0] = data[0][data[0].index.isin(ppi_genes)]
+    data[1] = data[1][data[1].index.isin(ppi_genes)]
+    data[2] = data[2][data[2].index.isin(ppi_genes)]
+    data[0].sort_index(inplace=True)
+    data[1].sort_index(inplace=True)
+    data[2].sort_index(inplace=True)
+    ppi = ppi[ppi.iloc[:,0].isin(ppi_genes)&ppi.iloc[:,1].isin(ppi_genes)]
+
+    print(len(set(ppi.values[:,0].flatten())|set(ppi.values[:,1].flatten())),len(data[0]))
+    
+    if not os.path.exists('./gene_overlapped/'):
+        os.makedirs('./gene_overlapped/')
+    if not os.path.exists('./gene_overlapped/' + cancer + '/'):
+        os.makedirs('./gene_overlapped/' + cancer + '/')
+
+    if not os.path.exists('./gene_sample_overlapped/'):
+        os.makedirs('./gene_sample_overlapped/')
+    if not os.path.exists('./gene_sample_overlapped/' + cancer):
+        os.makedirs('./gene_sample_overlapped/' + cancer)
+
+    ppi.to_csv('./gene_sample_overlapped/' + cancer + '/ppi.csv',index=None,header=None)
+    data[0].to_csv('./gene_overlapped/' + cancer + '/mut.csv')
+    data[1].to_csv('./gene_overlapped/' + cancer + '/methy.csv')
+    data[2].to_csv('./gene_overlapped/' + cancer + '/expr.csv')
+
+#sample split
+    subtype_label = pd.read_csv('./datasets/' + cancer + '/' + cancer + '_subtype_label.csv',index_col=None,header=None)
+    subtype_label.dropna(inplace=True)
+    
+    data[1].columns = [col.replace('.', '-')[0:15] for col in data[1].columns]
+    data[2].columns = [col.replace('.', '-')[0:15] for col in data[2].columns]
+
+    subtype_data = list()
+    for i in range(len(subtypes[cancer])):
+        subtype_data.append(subtype_label[subtype_label.iloc[:,1]==subtypes[cancer][i]])
+    
+    for i in range(len(subtypes[cancer])):
+        if not os.path.exists('./gene_overlapped/' + cancer + '/' + subtypes[cancer][i]):
+            os.makedirs('./gene_overlapped/' + cancer + '/' + subtypes[cancer][i])
+        data[0].iloc[:,data[0].columns.isin(subtype_data[i].iloc[:,0].values)].to_csv('./gene_overlapped/'+cancer+'/'+subtypes[cancer][i]+'/mut.csv')
+        data[1].iloc[:,data[1].columns.isin(subtype_data[i].iloc[:,0].values)].to_csv('./gene_overlapped/'+cancer+'/'+subtypes[cancer][i]+'/methy.csv')
+        data[2].iloc[:,data[2].columns.isin(subtype_data[i].iloc[:,0].values)].to_csv('./gene_overlapped/'+cancer+'/'+subtypes[cancer][i]+'/expr.csv')
+
+#sample_split_overlap
+
+for cancer in cancers:
+    for i in range(len(subtypes[cancer])):
+        data = list()
+        data.append(pd.read_csv('./gene_overlapped/'+cancer+'/'+subtypes[cancer][i]+'/mut.csv',header=0,index_col=0))
+        data.append(pd.read_csv('./gene_overlapped/'+cancer+'/'+subtypes[cancer][i]+'/methy.csv',header=0,index_col=0))
+        data.append(pd.read_csv('./gene_overlapped/'+cancer+'/'+subtypes[cancer][i]+'/expr.csv',header=0,index_col=0))
+        
+        data[1] = data[1].iloc[:,data[1].columns.isin(data[0].columns)]
+        data[2] = data[2].iloc[:,data[2].columns.isin(data[1].columns)]
+        data[0] = data[0].iloc[:,data[0].columns.isin(data[2].columns)]
+        data[1] = data[1].iloc[:,data[1].columns.isin(data[2].columns)]
+    
+        for j in range(len(data)):
+            data[j] = data[j].sort_index(axis=1)
+        print(data[0].shape,data[1].shape,data[2].shape)    
+
+
+        if not os.path.exists('./gene_sample_overlapped/' + cancer + '/' + subtypes[cancer][i]):
+            os.makedirs('./gene_sample_overlapped/' + cancer + '/' + subtypes[cancer][i])
+        data[0].to_csv('./gene_sample_overlapped/' + cancer+'/' + subtypes[cancer][i] + '/mut.csv')
+        data[1].to_csv('./gene_sample_overlapped/' + cancer+'/' + subtypes[cancer][i] + '/methy.csv')
+        data[2].to_csv('./gene_sample_overlapped/' + cancer+'/' + subtypes[cancer][i] + '/expr.csv')
+shutil.rmtree('./gene_overlapped')
---
+++ exclusivity_network.py
@@ -0,0 +1,58 @@
+import pandas as pd
+import numpy as np
+from functools import partial
+import networkx as nx
+import warnings
+import os
+from utils import get_edge_weight, cancers, subtypes
+
+warnings.filterwarnings('ignore', category=RuntimeWarning)
+
+
+for c, cancer in enumerate(cancers):
+    ppi = pd.read_csv('./gene_sample_overlapped/'+cancer+'/ppi.csv',header=None,index_col=None)
+
+    data_mut = list()
+    data_methy = list()
+    data_expr = list()
+    for s, subtype in enumerate(subtypes[cancer]):
+        data_mut.append(pd.read_csv('./gene_sample_overlapped/'+cancer+'/'+subtype+'/mut.csv',header=0,index_col=0))
+        data_methy.append(pd.read_csv('./gene_sample_overlapped/'+cancer+'/'+subtype+'/0.05/methy_mut.csv',header=0,index_col=0))
+        data_expr.append(pd.read_csv('./gene_sample_overlapped/'+cancer+'/'+subtype+'/0.05/expr_mut.csv',header=0,index_col=0))
+
+    def idconv(idtable,idx):
+        return idtable.loc[np.array(idx)].values.flatten()
+
+    ppi_symbol_idx = pd.DataFrame(range(len(data_mut[0].index)),index=data_mut[0].index)
+    symbol2idx = partial(idconv, ppi_symbol_idx)
+    ppi_idx = pd.DataFrame([symbol2idx(ppi.iloc[:,0]),symbol2idx(ppi.iloc[:,1])]).T.to_numpy()
+
+    for s, subtype in enumerate(subtypes[cancer]):
+        data_mut[s] = data_mut[s].values
+        data_methy[s] = data_methy[s].values
+        data_expr[s] = data_expr[s].values
+
+    edge_exclusivity = np.zeros((len(ppi_idx),len(subtypes[cancer])))
+    for s, subtype in enumerate(subtypes[cancer]):
+        edge_exclusivity[:,s] = get_edge_weight(data_mut[s], ppi_idx) + get_edge_weight(data_methy[s], ppi_idx) + get_edge_weight(data_expr[s], ppi_idx)
+
+    for s, subtype in enumerate(subtypes[cancer]):
+        exclusivity_others = np.delete(edge_exclusivity, s, axis=1)
+        exclusive_edges = (edge_exclusivity[:,s] > exclusivity_others.std(axis=1) + exclusivity_others.mean(axis=1)).astype(int)
+        exclusivity_network = ppi.copy()
+        exclusivity_network['Weight'] = edge_exclusivity[:,s]
+        exclusivity_network = exclusivity_network.iloc[(exclusive_edges==1)]
+        if len(exclusivity_network) > 0:
+            G = nx.Graph()
+            G.add_edges_from(exclusivity_network.values[:,0:2])
+            exclusivity_network_genes = sorted(nx.connected_components(G), key=len, reverse=True)[0]
+            exclusivity_network_genes = np.array(list(exclusivity_network_genes))
+            exclusivity_network = exclusivity_network.iloc[np.isin(exclusivity_network.values[:,0],exclusivity_network_genes),:]
+            exclusivity_network = exclusivity_network.iloc[np.isin(exclusivity_network.values[:,1],exclusivity_network_genes),:]
+            print('cancer {} subtype {}: {} exclusive edge(s).'.format(cancer,subtype,len(exclusivity_network)))
+            if not os.path.exists('./exclusivity_network/'):
+                os.makedirs('./exclusivity_network/')
+            exclusivity_network.to_csv('./exclusivity_network/' + cancer + '_' + subtype + '.csv',index=None)
+        
+        else:
+            print('cancer {} subtype {}: 0 exclusive edge.'.format(cancer,subtype))
